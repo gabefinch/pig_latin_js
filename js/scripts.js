e22072e30d4663365e37c08b98c5e853562f9f45
Gabe <gabefinch@gmail.com>
2015-02-09 16:57:21 -0800

    Adds 5 specs and meets them

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,50 @@
-var pigLatin = function(phrase){
-  
+var piggify = function(word) {
+  var caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var capitalized = (caps.indexOf(word[0]) !== -1)
+  word = word.toLowerCase();
+
+  if (word[0].concat(word[1]) === "qu") {
+    var first_letter = "qu";
+    var second_letter = word[2];
+    var start_length = 3;
+  }
+  else if (word[1].concat(word[2]) === "qu") {
+    var first_letter = word[0];
+    var second_letter = "qu";
+    var start_length = 3;
+  }
+  else {
+    var first_letter = word[0];
+    var second_letter = word[1];
+    var start_length = 2;
+  }
+
+  var vowels = 'aeiou'
+  if ((vowels.indexOf(first_letter) === -1) && (vowels.indexOf(second_letter) === -1))  {
+    var mod_word = word.slice(start_length).concat(first_letter).concat(second_letter);
+  }
+  else if (vowels.indexOf(first_letter) === -1) {
+    var mod_word = word.slice(start_length-1).concat(first_letter);
+  }
+  else {
+    var mod_word = word;
+  }
+
+  if (capitalized) {
+    mod_word = mod_word[0].toUpperCase() + mod_word.slice(1);
+  }
+
+  mod_word = mod_word.concat("ay");
+  return mod_word;
+
 };
 
-//
-// $(document).ready(function(){
-//   $("form#queen-attack").submit(function(event){
-//     var from_x = parseInt($("#from-x").val());
-//     var from_y = parseInt($("input#from-y").val());
-//     var to_x = parseInt($("input#to-x").val());
-//     var to_y = parseInt($("input#to-y").val());
-//     var from = [from_x,from_y];
-//     var to = [to_x,to_y];
-// // debugger;
-//     var result = queenAttack(from,to);
-//     if (result === true) {
-//       $(".answer").text("Go ahead schmuck, that is a legal move!");
-//     }
-//     else if (result === false) {
-//       $(".answer").text("FAIL!");
-//     }
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
 
-
-
-
-// $(document).ready(function() {
-//   $("form#leap-year").submit(function(event) {
-//     var year = parseInt($("input#year").val());
-//     var result = leapYear(year);
-//
-//     $('.year').text(year);
-//     if(!result) {
-//       $(".not").text("not");
-//     }
-//     else if (result) {
-//       $(".not").text("");
-//     }
-//
-//     $("#result").show();
-//     event.preventDefault();
-//
-//   });
-// });
+var pigLatin = function(phrase) {
+  var words = phrase.split(" ");
+  var mod_words = [];
+  words.forEach(function(word) {
+    mod_words.push(piggify(word));
+  });
+  return mod_words.join(" ");
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,27 @@
 describe('pigLatin', function() {
 
   it('works for single words', function() {
-    expect("punish").to.equal("unishpay");
+    expect(pigLatin("answer")).to.equal("answeray");
+  });
+
+  it('works for words which start with consonants', function() {
+    expect(pigLatin("police")).to.equal("olicepay");
+  });
+
+  it('works for words which start with 2 consonants', function() {
+    expect(pigLatin("brackish")).to.equal("ackishbray");
+  });
+
+  it('words for words using Q and U in succession if they are in the first syllable and following a first letter that contains only right angles', function() {
+    expect(pigLatin("squeal")).to.equal("ealsquay");
+  });
+
+  it('breaks an entire sentence down and does the above to each word', function() {
+    expect(pigLatin("howdy partner")).to.equal("owdyhay artnerpay");
+  });
+
+  it('Does the above and deals with capitalization', function() {
+    expect(pigLatin("Howdy there Partner")).to.equal("Owdyhay erethay Artnerpay");
   });
 
 
